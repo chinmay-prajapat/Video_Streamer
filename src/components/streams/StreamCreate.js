76330d74801c3e3f75aa694ac4e6d5d32ed7400c
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-10-03 17:50:02 +0530

    Components added

---
+++ src/components/streams/StreamCreate.js
@@ -0,0 +1,5 @@
+import React from "react";
+const StreamCreate = () => {
+  return <div>StreamCreate</div>;
+};
+export default StreamCreate;
---
+++ src/components/streams/StreamEdit.js
@@ -0,0 +1,5 @@
+import React from "react";
+const StreamEdit = () => {
+  return <div>StreamEdit</div>;
+};
+export default StreamEdit;
