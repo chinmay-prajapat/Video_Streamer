207aed41433ab5a5be8d42911632a784c6ca7169
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-10-03 22:37:41 +0530

    OAuth added

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,16 +1,35 @@
 import React, { Component } from "react";
 require("dotenv").config();
 class GoogleAuth extends Component {
+  state = { isSignedIn: null };
   componentDidMount() {
     window.gapi.load("client:auth2", () => {
-      window.gapi.client.init({
-        clientId: process.env.REACT_APP_API_KEY,
-        scope: "email",
-      });
+      window.gapi.client
+        .init({
+          clientId: process.env.REACT_APP_API_KEY,
+          scope: "email",
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.auth.isSignedIn.listen(this.onAuthChange);
+        });
     });
   }
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I dont know</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in</div>;
+    } else {
+      return <div>I am not signed in</div>;
+    }
+  }
   render() {
-    return <div>Google Auth</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 export default GoogleAuth;
