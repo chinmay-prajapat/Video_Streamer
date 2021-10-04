c911ccea122cbcc1844671cbe365b87c3999a5f2
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-10-04 17:07:30 +0530

    action types added as CONSTANTS

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,11 @@
+import { SIGN_OUT, SIGN_IN } from "./types";
 export const signIn = () => {
   return {
-    type: "SIGN_IN",
+    type: SIGN_IN,
   };
 };
 export const signOut = () => {
   return {
-    type: "SIGN_OUT",
+    type: SIGN_OUT,
   };
 };
--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,11 +1,12 @@
+import { SIGN_OUT, SIGN_IN } from "../actions/types";
 const INITIAL_STATE = {
   isSignedIn: null,
 };
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case "SIGN_IN":
+    case SIGN_IN:
       return { ...state, isSignedIn: true };
-    case "SIGN_OUT":
+    case SIGN_OUT:
       return { ...state, isSignedIn: false };
     default:
       return state;
