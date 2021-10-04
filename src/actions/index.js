aeb7752aefc0d9da1e3562fb9555067d3634cea2
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-10-04 17:16:08 +0530

    userId added

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,8 @@
 import { SIGN_OUT, SIGN_IN } from "./types";
-export const signIn = () => {
+export const signIn = (userId) => {
   return {
     type: SIGN_IN,
+    payload: userId,
   };
 };
 export const signOut = () => {
