db2bc786b1518004f8544e7e85ff524b0655ba35
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-10-05 17:58:12 +0530

    Redux forms

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from "redux";
+import { reducer as formReducer } from "redux-form";
 import AuthReducer from "./AuthReducer";
 export default combineReducers({
   auth: AuthReducer,
+  form: formReducer,
 });
