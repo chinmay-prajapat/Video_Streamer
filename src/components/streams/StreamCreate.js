3445ab5ef8a2bf8d5d56b2b9469c64e304b25e10
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-10-05 18:09:12 +0530

    Form submition by redux submit function

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,14 +1,30 @@
 import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
 class StreamCreate extends Component {
-  renderInput({ input }) {
-    return <input {...input} />;
+  renderInput({ input, label }) {
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} />
+      </div>
+    );
+  }
+  onSubmit(formValues) {
+    console.log(formValues);
   }
   render() {
     return (
-      <form>
-        <Field name="title" component={this.renderInput} />
-        <Field name="description" component={this.renderInput} />
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form"
+      >
+        <Field name="title" label="Enter Title" component={this.renderInput} />
+        <Field
+          name="description"
+          label="Enter Description"
+          component={this.renderInput}
+        />
+        <button className="ui button primary">Submit</button>
       </form>
     );
   }
